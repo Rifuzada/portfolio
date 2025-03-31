317256cf0d37ebff2ebb666f3bb8656e48d9d712
Rifu <rifuperez@gmail.com>
2025-03-31 17:25:45 -0300

    creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,101 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="flex justify-center items-center w-full h-screen">
+      <div className="grid grid-cols-4 grid-rows-3 gap-2">
+        <div id="title" className="relative cursor-pointer w-60 hover:scale-110 hover:z-10 z-0 ease-in duration-200 row-span-1 col-span-1 overflow-hidden group">
+          {/* Efeito de reflexo */}
+          <div className="absolute inset-0 bg-gradient-to-br from-white/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200"></div>
+          <img className="max-w-full h-auto" src="https://i.pinimg.com/736x/e9/c8/3c/e9c83cafc9621af76876a95a21c78dc6.jpg" alt="image description" />
+          <p className="absolute inset-0 flex items-end justify-normal text-lg text-white ml-3">
+            Play Skyrim
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        
+        {/* Bloco grande (LoLData) com efeito de reflexo */}
+        <div id="title" className="relative bg-[#088c04] hover:scale-110 hover:z-10 z-0 ease-in duration-200 cursor-pointer col-span-2 row-span-2 flex flex-col justify-center text-center p-4 overflow-hidden group">
+          {/* Efeito de reflexo */}
+          <div className="absolute inset-0 bg-gradient-to-br from-white/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200"></div>
+          <div className="flex justify-center items-center mt-5"> {/* Adicionando margem superior para centralizar melhor */}
+              <img className="max-w-30 h-auto" src="https://img.icons8.com/?size=100&id=fJp7hepMryiw&format=png&color=FFFFFF" alt="image description" />
+            </div>
+          <p className="absolute inset-0 flex items-end justify-normal text-lg text-white ml-3">
+            X
+          </p>
+        </div>
+        
+          <div id="title" className=" bg-[#088c04] relative cursor-pointer w-60 hover:scale-110 hover:z-10 z-0 ease-in duration-200 row-span-1 col-span-1 overflow-hidden group">
+            {/* Efeito de reflexo */}
+            <div className="absolute inset-0 bg-gradient-to-br from-white/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200"></div>
+            <div className="flex justify-center items-center mt-5"> {/* Adicionando margem superior para centralizar melhor */}
+              <img className="max-w-30 h-auto" src="https://img.icons8.com/?size=100&id=12464&format=png&color=FFFFFF" alt="image description" />
+            </div>
+            <p className="absolute inset-0 flex items-end justify-normal text-lg text-white ml-3">
+              SoundCloud
+            </p>
+          </div>
+          <div id="title" className=" bg-[#088c04] relative cursor-pointer w-60 hover:scale-110 hover:z-10 z-0 ease-in duration-200 row-span-1 col-span-1 overflow-hidden group">
+            {/* Efeito de reflexo */}
+            <div className="absolute inset-0 bg-gradient-to-br from-white/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200"></div>
+            <div className="flex justify-center items-center mt-5"> {/* Adicionando margem superior para centralizar melhor */}
+              <img className="max-w-30 h-auto" src="/letterboxd.png" alt="imagem de descrição" />
+            </div>
+            <p className="absolute inset-0 flex items-end justify-normal text-lg text-white ml-3">
+              Letterboxd
+            </p>
+          </div>
+          <div id="title" className=" bg-[#088c04] relative cursor-pointer w-60 hover:scale-110 hover:z-10 z-0 ease-in duration-200 row-span-1 col-span-1 overflow-hidden group">
+            {/* Efeito de reflexo */}
+            <div className="absolute inset-0 bg-gradient-to-br from-white/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200"></div>
+            <div className="flex justify-center items-center mt-5"> {/* Adicionando margem superior para centralizar melhor */}
+              <img className="max-w-30 h-auto" src="https://img.icons8.com/?size=100&id=118638&format=png&color=FFFFFF" alt="image description" />
+            </div>
+            <p className="absolute inset-0 flex items-end justify-normal text-lg text-white ml-3">
+              Tiktok
+            </p>
+          </div>
+          <div id="title" className=" bg-[#088c04] relative cursor-pointer w-60 hover:scale-110 hover:z-10 z-0 ease-in duration-200 row-span-1 col-span-1 overflow-hidden group">
+            {/* Efeito de reflexo */}
+            <div className="absolute inset-0 bg-gradient-to-br from-white/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200"></div>
+            <div className="flex justify-center items-center mt-5"> {/* Adicionando margem superior para centralizar melhor */}
+              <img className="max-w-30 h-auto" src="https://img.icons8.com/?size=100&id=12598&format=png&color=FFFFFF" alt="image description" />
+            </div>
+            <p className="absolute inset-0 flex items-end justify-normal text-lg text-white ml-3">
+              Github
+            </p>
+          </div>
+          <div id="title" className=" bg-[#088c04] relative cursor-pointer w-60 hover:scale-110 hover:z-10 z-0 ease-in duration-200 row-span-1 col-span-1 overflow-hidden group">
+            {/* Efeito de reflexo */}
+            <div className="absolute inset-0 bg-gradient-to-br from-white/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200"></div>
+            <div className="flex justify-center items-center mt-5"> {/* Adicionando margem superior para centralizar melhor */}
+              <img className="max-w-30 h-auto" src="https://img.icons8.com/?size=100&id=99999&format=png&color=FFFFFF" alt="image description" />
+            </div>
+            <p className="absolute inset-0 flex items-end justify-normal text-lg text-white ml-3">
+              Steam
+            </p>
+          </div>
+          <div id="title" className=" bg-[#088c04] relative cursor-pointer w-60 hover:scale-110 hover:z-10 z-0 ease-in duration-200 row-span-1 col-span-1 overflow-hidden group">
+            {/* Efeito de reflexo */}
+            <div className="absolute inset-0 bg-gradient-to-br from-white/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200"></div>
+            <div className="flex justify-center items-center mt-6"> {/* Adicionando margem superior para centralizar melhor */}
+              <img className="max-w-30 h-auto" src="https://img.icons8.com/?size=100&id=30888&format=png&color=FFFFFF" alt="image description" />
+            </div>
+            <p className="absolute inset-0 flex items-end justify-normal text-lg text-white ml-3">
+              Discord
+            </p>
+          </div>
+          <div id="title" className=" bg-[#088c04] relative cursor-pointer w-60 hover:scale-110 hover:z-10 z-0 ease-in duration-200 row-span-1 col-span-1 overflow-hidden group">
+            {/* Efeito de reflexo */}
+            <div className="absolute inset-0 bg-gradient-to-br from-white/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200"></div>
+            <div className="flex justify-center items-center mt-5"> {/* Adicionando margem superior para centralizar melhor */}
+              <img className="max-w-30 h-auto" src="https://img.icons8.com/win10/512/FFFFFF/instagram-new.png" alt="image description" />
+            </div>
+            <p className="absolute inset-0 flex items-end justify-normal text-lg text-white ml-3">
+              Instagram
+            </p>
+          </div>
+          
+      </div>
+    </main>
   );
-}
+};
